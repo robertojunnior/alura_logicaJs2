35392a6731910a65c21fd71ffec3a7329f849954
roberjunnior <roberjunior.dev@gmail.com>
2023-11-03 03:04:15 -0300

    Update

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 alert("Boas vindas ao jogo do número secreto!");
-let numeroSecreto = 5;
+let numeroSecreto = 15;
 let chute = prompt("Escolha um número de 1 a 30!");
 
+// se chute for igual número secreto
 if (chute == numeroSecreto) {
-  console.log("Parabéns, você acertou o número secreto (5)!");
+  alert(`Parabéns, você acertou! O ${numeroSecreto} é o número secreto!`);
+} else if (chute < numeroSecreto) {
+  alert("Tente outra vez, seu chute foi menor que o número secreto!");
+} else {
+  alert("Tente outra vez, seu chute foi maior que o número secreto!");
 }
-
-console.log("Olá");
