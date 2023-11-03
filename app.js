1bde9d1d6eda840a7503a04aaf260dd6cdecc29a
roberjunnior <roberjunior.dev@gmail.com>
2023-11-03 02:45:02 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Boas vindas ao jogo do número secreto!");
+let numeroSecreto = 5;
+let chute = prompt("Escolha um número de 1 a 30!");
+
+if (chute == numeroSecreto) {
+  console.log("Parabéns, você acertou o número secreto (5)!");
+}
+
+console.log("Olá");
