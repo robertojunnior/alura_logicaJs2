25267864350024f12196cae6f955411e2c29c79b
roberjunnior <roberjunior.dev@gmail.com>
2023-11-05 23:14:52 -0300

    update aula_logica 3 JS

---
+++ logica-js-aula_3/app.js
@@ -0,0 +1,36 @@
+alert("Boas vindas ao jogo do número secreto!");
+let numeroSecreto = 15;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto
+while (chute != numeroSecreto) {
+  chute = prompt("Escolha um número de 1 a 20!");
+
+  // se chute for igual número secreto
+  if (chute == numeroSecreto) {
+    break;
+  } else {
+    if (chute > numeroSecreto) {
+      alert(`Tente outra vez, ${chute} é maior que o número secreto!`);
+    } else {
+      alert(`Tente outra vez, ${chute} é menor que o número secreto!`);
+    }
+    // tentativas = tentativas + 1, vai fora do else para somar a tent do acerto tbm
+    tentativas++;
+  }
+  console.log(tentativas);
+}
+
+if (tentativas > 1) {
+  alert(
+    `Parabéns, você acertou em ${tentativas} tentativas! O número secreto é o ${numeroSecreto}!`
+  );
+  alert("Pressione a tecla F5 para recomeçar!");
+} else {
+  alert(
+    `Parabéns, você acertou na primeira tentativa! O número secreto é o ${numeroSecreto}!`
+  );
+  alert("Pressione a tecla F5 para recomeçar!");
+}
