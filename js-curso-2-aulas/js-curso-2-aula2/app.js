ffbef95e49a5c99fc262cd20edb6ede493e8652e
roberjunnior <roberjunior.dev@gmail.com>
2023-11-09 03:22:48 -0300

    update aula 2 curso js2

---
+++ js-curso-2-aulas/js-curso-2-aula2/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Jodo do número Secreto";
+
+let paragrafo = document.querySelector("p");
+paragrafo.innerHTML = "Escolha o número entre 1 e 10.";
+
+function verificarChute() {
+  console.log("O botão foi clicado!");
+}
