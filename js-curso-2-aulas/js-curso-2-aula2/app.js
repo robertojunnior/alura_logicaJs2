fd0c1a788b0b9ff3b4002fb23d45a4fc6e1d6799
roberjunnior <roberjunior.dev@gmail.com>
2023-11-09 14:20:37 -0300

    update aula 2

--- js-curso-2-aulas/js-curso-2-aula2/app.js
+++ js-curso-2-aulas/js-curso-2-aula2/app.js
@@ -4,6 +4,12 @@ titulo.innerHTML = "Jodo do número Secreto";
 let paragrafo = document.querySelector("p");
 paragrafo.innerHTML = "Escolha o número entre 1 e 10.";
 
+function exibirTextoNaTela() {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+exibirTextoNaTela();
+
 function verificarChute() {
   console.log("O botão foi clicado!");
 }
