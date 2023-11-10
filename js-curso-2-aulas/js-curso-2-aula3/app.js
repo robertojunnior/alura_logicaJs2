fdc3e6b24c661410bbd74ee6c8468dbc2c76cf95
roberjunnior <roberjunior.dev@gmail.com>
2023-11-10 19:52:05 -0300

    update - curso 2 - aula 3 (Reiniciando o jogo)

---
+++ js-curso-2-aulas/js-curso-2-aula3/app.js
@@ -0,0 +1,19 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+exibirTextoNaTela("h1", "Jodo do número Secreto");
+exibirTextoNaTela("p", "Escolha o número entre 1 e 10.");
+
+function verificarChute() {
+  let chute = document.querySelector("input").value;
+  console.log(chute == numeroSecreto);
+  console.log(numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
+}
