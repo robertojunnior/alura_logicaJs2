672c63303b62281e5816ec098cc431e893dd4f1d
roberjunnior <roberjunior.dev@gmail.com>
2023-11-13 01:53:27 -0300

    update - Funções jogo numero secreto

--- js-curso-2-aulas/js-curso-2-aula3/app.js
+++ js-curso-2-aulas/js-curso-2-aula3/app.js
@@ -1,17 +1,30 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
 }
-
 exibirTextoNaTela("h1", "Jodo do número Secreto");
 exibirTextoNaTela("p", "Escolha o número entre 1 e 10.");
 
 function verificarChute() {
-  let chute = document.querySelector("input").value;
-  console.log(chute == numeroSecreto);
-  console.log(numeroSecreto);
+  let chute = parseInt(document.querySelector("input").value);
+
+  if(chute == numeroSecreto) {
+    exibirTextoNaTela("h1", "Acertou!");
+    let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+    exibirTextoNaTela("p", mensagemTentativas);
+
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela("p", "O número secreto é menor!");
+    } else {
+      exibirTextoNaTela("p", "O número secreto é maior!");
+    }
+    tentativas++
+  }
 }
 
 function gerarNumeroAleatorio() {
