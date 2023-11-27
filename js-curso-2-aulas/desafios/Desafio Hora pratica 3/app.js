42d4dc623f3c4e21c7de17b1c2b23a5636de6837
roberjunnior <roberjunior.dev@gmail.com>
2023-11-27 03:40:18 -0300

    update - Desafio hora pratica 3

---
+++ js-curso-2-aulas/desafios/Desafio Hora pratica 3/app.js
@@ -0,0 +1,16 @@
+//Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+//Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+console.log(linguagensDeProgramacao);
+//Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+console.log(linguagensDeProgramacao);
+//Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaNomes = ['Joao', 'Jose', 'Joana'];
+console.log(listaNomes[0]);
+//Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(listaNomes[1]);
+//Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(listaNomes[2]);
+
